b068cd6c623edb6653ce4fbea234f05ef7ef79b3
Santos van_der_Wansem <srrvdwansem@gmail.com>
2022-06-30 23:19:08 +0200

    Finished articles

--- src/js/script.js
+++ src/js/script.js
@@ -31,13 +31,19 @@ fetch('https://api.nytimes.com/svc/topstories/v2/world.json?api-key=3Ne3OobjMQy6
                 image: document.querySelector(`#article_image_0${i}`),
                 metadata: document.querySelector(`#article_metadata_0${i}`),
                 title: document.querySelector(`#article_title_0${i}`),
+                description: document.querySelector(`#article_description_0${i}`),
+                date: document.querySelector('.date'),
+                ref: document.querySelector(`#article_link_0${i}`),
             };
             articleCardData = {
                 image: jsonData.results[i].multimedia[0].url,
                 title: jsonData.results[i].title,
                 description: jsonData.results[i].abstract,
-                date: jsonData.results[i].published_date.substring(0, 10),
+                date: jsonData.results[i].updated_date.substring(5, 10),
                 url: jsonData.results[i].url,
+                byline: jsonData.results[i].byline,
+                geo: jsonData.results[i].geo_facet,
+                des: jsonData.results[i].des_facet,
             };
             // Image
             articleCard.image.src = articleCardData.image;
@@ -45,6 +51,17 @@ fetch('https://api.nytimes.com/svc/topstories/v2/world.json?api-key=3Ne3OobjMQy6
             // Title
             articleCard.title.innerHTML = `<h6>${articleCardData.title}</h6>`;
             // Description
+            articleCard.description.innerHTML += `<p>${articleCardData.description}</p>`;
+            // Date
+            articleCard.date.innerHTML = `<p>${articleCardData.date}</p>`;
+            // Metadata
+            articleCard.metadata.innerHTML = `<p>${articleCardData.des[0]}</p>`;
+            // Link
+            let a = document.createElement('a'); // Create a <a> element
+            a.href = articleCardData.url; // Set the href to the url
+            a.innerHTML = 'Read more'; // Set the text content to the url
+            a.id = `article_link_a_0${i}`; // Set the id to the url
+            articleCard.ref.appendChild(a); // Append the <a> to the <p>
         }
     })
     .catch((error) => {
